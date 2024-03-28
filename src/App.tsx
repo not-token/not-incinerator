41450e5c910912ba4110b5c843702d52d78f3ada
hz <gravatar.m2a16@simplelogin.com>
2024-03-28 22:31:20 +0200

    add vite

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,39 @@
 // -------------------------------------------------------- //
 
 import { useEffect, useRef, useState } from "react";
-import { AppConfig, UserSession, openContractCall, showConnect } from '@stacks/connect';
+import {
+  AppConfig,
+  UserSession,
+  openContractCall,
+  showConnect,
+} from "@stacks/connect";
 import { StacksMainnet } from "@stacks/network";
-import { uintCV, StacksTransaction, PostConditionMode, serializeCV , cvToValue } from '@stacks/transactions';
+import {
+  uintCV,
+  StacksTransaction,
+  PostConditionMode,
+  serializeCV,
+  cvToValue,
+} from "@stacks/transactions";
 
-import burnChrome from "./images/burn-chrome.png";
-import searchIcon from "./images/input-icon.png";
-import notWhiteX from "./images/whitex.png";
+import burnChrome from "./assets/burn-chrome.png";
+import searchIcon from "./assets/input-icon.png";
+import notWhiteX from "./assets/logo_circle_white.svg";
 
 import "./App.css";
 import { deserialize } from "@stacks/transactions/dist/cl";
-
 
 // --------------------------------------------- //
 // ------------- DECLARE CONSTANTS ------------- //
 // --------------------------------------------- //
 
-const appConfig = new AppConfig(['store_write', 'publish_data']);
+const appConfig = new AppConfig(["store_write", "publish_data"]);
 const userSession = new UserSession({ appConfig });
 const mainnet = new StacksMainnet();
 
-const contractAddress = 'SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V';
-const contractName = 'not-incinerator-v3';
-const functionNameBurn = 'burn-nothing';
+const contractAddress = "SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V";
+const contractName = "not-incinerator-v3";
+const functionNameBurn = "burn-nothing";
 const maxAmount = 10000000;
 
 const description = `By burning any amount of $NOT you'll receive a commemorative NFT in your wallet.
@@ -51,7 +61,7 @@ interface BurnerData {
 
 // Error modal component
 interface ErrorModalProps {
-  reloadPage: () => void; 
+  reloadPage: () => void;
 }
 
 const ErrorModal: React.FC<ErrorModalProps> = ({ reloadPage }) => {
@@ -64,9 +74,17 @@ const ErrorModal: React.FC<ErrorModalProps> = ({ reloadPage }) => {
 };
 
 // Burners List modal component
-function BurnersModal({ burnersList, onClose }: { burnersList: BurnerData[], onClose: () => void }) {
-  const handlePanelClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    event.stopPropagation(); 
+function BurnersModal({
+  burnersList,
+  onClose,
+}: {
+  burnersList: BurnerData[];
+  onClose: () => void;
+}) {
+  const handlePanelClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
   };
 
   return (
@@ -74,11 +92,12 @@ function BurnersModal({ burnersList, onClose }: { burnersList: BurnerData[], onC
       <div className="burners-panel-rect" onClick={handlePanelClick}>
         <div className="burners-list">
           <h2>Burners List</h2>
-          <div className="tot-line"/>
+          <div className="tot-line" />
           <ul>
             {burnersList.map((burner, index) => (
               <li key={index}>
-                {burner.burnerAddress}     -     {burner.burnerAmount.toLocaleString()} $NOT
+                {burner.burnerAddress} - {burner.burnerAmount.toLocaleString()}{" "}
+                $NOT
               </li>
             ))}
           </ul>
@@ -88,9 +107,7 @@ function BurnersModal({ burnersList, onClose }: { burnersList: BurnerData[], onC
   );
 }
 
-
 function App() {
-
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [isInputHovered, setIsInputHovered] = useState(false);
@@ -98,12 +115,12 @@ function App() {
   const [inputText, setInputText] = useState("");
 
   const [authenticated, setAuthenticated] = useState(false);
-  const [userPrincipal, setUserPrincipal] = useState('');
+  const [userPrincipal, setUserPrincipal] = useState("");
   const [amount, setAmount] = useState(0);
   const [balance, setBalance] = useState(0);
   const [totBurned, setTotBurned] = useState(0);
-  const [totBurns, setTotBurns] = useState(0)
-  const [burnersList, setBurnersList] = useState<BurnerData[]>([])
+  const [totBurns, setTotBurns] = useState(0);
+  const [burnersList, setBurnersList] = useState<BurnerData[]>([]);
 
   const [showBurnersModal, setShowBurnersModal] = useState(false);
   const [showErrorModal, setShowErrorModal] = useState(false);
@@ -112,7 +129,6 @@ function App() {
     window.location.reload();
   };
 
-  
   // --------------------------------------------- //
   // ----------------- FUNCTIONS ----------------- //
   // --------------------------------------------- //
@@ -121,10 +137,10 @@ function App() {
   function authenticate() {
     showConnect({
       appDetails: {
-        name: 'Nothing Incinerator',
-        icon: 'https://bafkreieug75i7f74at6gailpsox52lgs2ct7zccht5nobik3giv4opkeuu.ipfs.dweb.link/',
+        name: "Nothing Incinerator",
+        icon: "https://bafkreieug75i7f74at6gailpsox52lgs2ct7zccht5nobik3giv4opkeuu.ipfs.dweb.link/",
       },
-      redirectTo: '/',
+      redirectTo: "/",
       onFinish: () => {
         setAuthenticated(true);
         const userData = userSession.loadUserData();
@@ -138,15 +154,23 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://api.mainnet.hiro.so/extended/v1/address/' + userPrincipal + '/balances');
+        const response = await fetch(
+          "https://api.mainnet.hiro.so/extended/v1/address/" +
+            userPrincipal +
+            "/balances"
+        );
         const data = await response.json();
         if (
           data.fungible_tokens &&
-          data.fungible_tokens["SP32AEEF6WW5Y0NMJ1S8SBSZDAY8R5J32NBZFPKKZ.nope::NOT"]
+          data.fungible_tokens[
+            "SP32AEEF6WW5Y0NMJ1S8SBSZDAY8R5J32NBZFPKKZ.nope::NOT"
+          ]
         ) {
           setBalance(
             parseInt(
-              data.fungible_tokens["SP32AEEF6WW5Y0NMJ1S8SBSZDAY8R5J32NBZFPKKZ.nope::NOT"].balance
+              data.fungible_tokens[
+                "SP32AEEF6WW5Y0NMJ1S8SBSZDAY8R5J32NBZFPKKZ.nope::NOT"
+              ].balance
             )
           );
         } else {
@@ -160,24 +184,26 @@ function App() {
     if (userPrincipal !== "") {
       fetchData();
     }
-
   }, [userPrincipal]);
 
   // Get Tot burned NOT function
   useEffect(() => {
     const fetchData2 = async () => {
       try {
-        const response = await fetch('https://api.mainnet.hiro.so/v2/contracts/call-read/SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V/not-incinerator-v3/get-burned-amount', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          body: JSON.stringify({
-            sender: 'SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V',
-            arguments: []
-          })
-        });
+        const response = await fetch(
+          "https://api.mainnet.hiro.so/v2/contracts/call-read/SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V/not-incinerator-v3/get-burned-amount",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              sender: "SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V",
+              arguments: [],
+            }),
+          }
+        );
 
         const data = await response.json();
         if (data.okay && data.result) {
@@ -199,7 +225,7 @@ function App() {
 
   const removeLeadingZeros = (hexString: string): string => {
     let index = 0;
-    while (hexString[index] === '0') {
+    while (hexString[index] === "0") {
       index++;
     }
     return hexString.substring(index);
@@ -207,21 +233,23 @@ function App() {
 
   // Get Tot Burns and Burners List data
   useEffect(() => {
-
     // Get Tot Burns
     const fetchData3 = async () => {
       try {
-        const response = await fetch('https://api.mainnet.hiro.so/v2/contracts/call-read/SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V/not-incinerator-v3/get-tot-burns', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          body: JSON.stringify({
-            sender: 'SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V',
-            arguments: []
-          })
-        });
+        const response = await fetch(
+          "https://api.mainnet.hiro.so/v2/contracts/call-read/SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V/not-incinerator-v3/get-tot-burns",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              sender: "SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V",
+              arguments: [],
+            }),
+          }
+        );
 
         const data = await response.json();
         if (data.okay && data.result) {
@@ -237,42 +265,43 @@ function App() {
     };
 
     // Get Burners List data
-    const fetchData4 = async (index: number): Promise<BurnerData> =>{
+    const fetchData4 = async (index: number): Promise<BurnerData> => {
       try {
-
         const clarityValue = uintCV(index);
         const serializedClarityValue = serializeCV(clarityValue);
 
-        let hexString = '';
+        let hexString = "";
         for (const byte of serializedClarityValue) {
-            hexString += ('0' + (byte & 0xFF).toString(16)).slice(-2);
+          hexString += ("0" + (byte & 0xff).toString(16)).slice(-2);
         }
 
-        const response = await fetch('https://api.mainnet.hiro.so/v2/contracts/call-read/SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V/not-incinerator-v3/get-burns-list', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          body: JSON.stringify({
-            sender: 'SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V',
-            arguments: [hexString]
-          })
-        });
+        const response = await fetch(
+          "https://api.mainnet.hiro.so/v2/contracts/call-read/SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V/not-incinerator-v3/get-burns-list",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify({
+              sender: "SPEMB0KQRD7PWKY2W2J2Y1Y6Q9YBJ702DWQADE7V",
+              arguments: [hexString],
+            }),
+          }
+        );
 
         const data = await response.json();
         if (data.okay && data.result) {
-         
           const clarityValue = deserialize(data.result);
           const valueResult = cvToValue(clarityValue);
           const { value } = valueResult;
-          const amount = value?.value?.amount?.value || ''; 
-          const maker = value?.value?.maker?.value || '';
+          const amount = value?.value?.amount?.value || "";
+          const maker = value?.value?.maker?.value || "";
           const burnerData: BurnerData = {
             burnerAddress: maker,
-            burnerAmount: parseInt(amount)
+            burnerAmount: parseInt(amount),
           };
-    
+
           return burnerData;
         } else {
           setShowErrorModal(true);
@@ -306,12 +335,20 @@ function App() {
     const inputAmount = parseFloat(input);
     let newInputText = "";
     if (balance < maxAmount) {
-      newInputText = input === "0" || input === "" ? "" : 
-        inputAmount <= balance ? inputAmount.toLocaleString() : balance.toLocaleString();
+      newInputText =
+        input === "0" || input === ""
+          ? ""
+          : inputAmount <= balance
+          ? inputAmount.toLocaleString()
+          : balance.toLocaleString();
     } else {
-      newInputText = input === "0" || input === "" ? "" : 
-        inputAmount <= maxAmount ? inputAmount.toLocaleString() : maxAmount.toLocaleString();
-    }  
+      newInputText =
+        input === "0" || input === ""
+          ? ""
+          : inputAmount <= maxAmount
+          ? inputAmount.toLocaleString()
+          : maxAmount.toLocaleString();
+    }
     setInputText(newInputText);
   };
 
@@ -332,17 +369,13 @@ function App() {
     setIsInputActive(false);
   };
 
-
   // Burn button function
   async function handleBurnClick() {
-
     if (!authenticated) {
       return;
     }
 
-    const functionArgs = [
-      uintCV(amount)
-    ];
+    const functionArgs = [uintCV(amount)];
 
     const options = {
       contractAddress: contractAddress,
@@ -350,16 +383,16 @@ function App() {
       functionName: functionNameBurn,
       functionArgs: functionArgs,
       appDetails: {
-        name: 'Not Incinerator',
-        icon: 'https://bafkreieug75i7f74at6gailpsox52lgs2ct7zccht5nobik3giv4opkeuu.ipfs.dweb.link/',
+        name: "Not Incinerator",
+        icon: "https://bafkreieug75i7f74at6gailpsox52lgs2ct7zccht5nobik3giv4opkeuu.ipfs.dweb.link/",
       },
       network: mainnet,
       postConditionMode: PostConditionMode.Allow,
       onFinish: (data: FinishData) => {
         const explorerTransactionUrl = `https://explorer.stacks.co/txid/${data.txId}?chain=mainnet`;
         const receiptTransactionUrl = `https://api.mainnet.hiro.so/extended/v1/tx/${data.txId}`;
-        console.log('View transaction receipt:', receiptTransactionUrl);
-        window.open(explorerTransactionUrl, '_blank');
+        console.log("View transaction receipt:", receiptTransactionUrl);
+        window.open(explorerTransactionUrl, "_blank");
       },
     };
     await openContractCall(options);
@@ -375,49 +408,63 @@ function App() {
     setShowBurnersModal(false);
   };
 
-  
   // ------------------------------------------- //
   // ------------- RENDER FUNCTION ------------- //
   // ------------------------------------------- //
 
   return (
     <div className="home-container">
-      
-      { /* Auth Button */}
+      {/* Auth Button */}
       {authenticated ? (
         <>
-          <button className="auth-button" disabled>{userPrincipal}</button>
+          <button className="auth-button" disabled>
+            {userPrincipal}
+          </button>
         </>
       ) : (
-        <button className="auth-button" onClick={authenticate}>Authenticate</button>
+        <button className="auth-button" onClick={authenticate}>
+          Authenticate
+        </button>
       )}
 
-      { /* Error Modal */}
+      {/* Error Modal */}
       {showErrorModal && <ErrorModal reloadPage={reloadPage} />}
 
-      { /* Total Burned Component */}
+      {/* Total Burned Component */}
       <div className="tot-wrapper2">
-      <img src={notWhiteX} alt="not" className="not-img" />
+        <img src={notWhiteX} alt="not" className="not-img" />
         <div className="tot-wrapper">
           <div className="tot-burned-text">Total $NOT incinerated</div>
-          <div className="tot-line"/>
+          <div className="tot-line" />
           <div className="tot-burned-text2">{totBurned.toLocaleString()}</div>
         </div>
         <img src={notWhiteX} alt="not" className="not-img" />
       </div>
 
-      { /* Burn Panel Component */}
+      {/* Burn Panel Component */}
       <div className="burn-panel-wrapper">
         <img src={burnChrome} alt="chrome1" className="burn-chrome" />
-        <div className="burn-panel-rect">         
+        <div className="burn-panel-rect">
           <div className="burn-panel-container">
             <div className="burn-title-text"> $NOT Incinerator</div>
             <div className="burn-panel-line" />
-            <div className="burn-descr-text" dangerouslySetInnerHTML={{ __html: description }} />
-            <div className="input-container" onMouseEnter={() => setIsInputHovered(true)} onMouseLeave={() => setIsInputHovered(false)} onClick={handleInputClick}>
-              <div className={`input-rect1 ${isInputHovered || isInputActive ? "active" : ""}`} />
+            <div
+              className="burn-descr-text"
+              dangerouslySetInnerHTML={{ __html: description }}
+            />
+            <div
+              className="input-container"
+              onMouseEnter={() => setIsInputHovered(true)}
+              onMouseLeave={() => setIsInputHovered(false)}
+              onClick={handleInputClick}
+            >
+              <div
+                className={`input-rect1 ${
+                  isInputHovered || isInputActive ? "active" : ""
+                }`}
+              />
               <div className="input-rect2" />
-              <div className="input-rect3" >
+              <div className="input-rect3">
                 <img src={searchIcon} alt="sIcon" className="input-icon" />
                 <input
                   ref={inputRef}
@@ -431,18 +478,28 @@ function App() {
                 />
               </div>
             </div>
-            <div className="balance-text">Balance: {balance.toLocaleString()}</div>
-            <div className="burn-button" onClick={handleBurnClick}>BURN</div>
-            <div className="burn-descr-text">You will burn {amount.toLocaleString()} $NOT</div>
+            <div className="balance-text">
+              Balance: {balance.toLocaleString()}
+            </div>
+            <div className="burn-button" onClick={handleBurnClick}>
+              BURN
+            </div>
+            <div className="burn-descr-text">
+              You will burn {amount.toLocaleString()} $NOT
+            </div>
           </div>
         </div>
       </div>
 
-      { /* Burners List Text-Button + Modal */}
-      <div className="burners-text" onClick={handleBurnersTextClick}>Show $NOT Burners list</div>
-      {showBurnersModal && <BurnersModal burnersList={burnersList} onClose={handleCloseModal} />}
+      {/* Burners List Text-Button + Modal */}
+      <div className="burners-text" onClick={handleBurnersTextClick}>
+        Show $NOT Burners list
+      </div>
+      {showBurnersModal && (
+        <BurnersModal burnersList={burnersList} onClose={handleCloseModal} />
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
